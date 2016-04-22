0b15f114c7183ffc22890b12b6d556d9e932ca84
Alex Sam <greeka@gmail.com>
2016-04-22 13:34:06 +0300

    Change config in Nucleo401RE_RTOS

---
+++ Nucleo401RE_RTOS/Inc/config.h
@@ -0,0 +1,10 @@
+#ifndef __config_H
+#define __config_H
+
+#define AS_CMD_BEGIN_CHAR1 0x41  // 'A'
+#define AS_CMD_BEGIN_CHAR2 0x54  // 'T'
+#define AS_CMD_BEGIN_CHAR3 0x2B  // '+'
+#define AS_CMD_LENGTH 100
+#define AS_CMD_END_CHAR 0x0D
+
+#endif /* __config_H */
